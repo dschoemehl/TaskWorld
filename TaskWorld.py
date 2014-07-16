c06c8213c2ec9e8bddbf1e08947457b4a0c1c062
David Schoemehl <david.schoemehl@asynchrony.com>
2014-07-16 16:32:49 -0500

    Broke task into a separate class and added a label

--- TaskWorld.py
+++ TaskWorld.py
@@ -3,15 +3,57 @@ from pyglet.gl import *
 
 window = pyglet.window.Window()
 
-# Setup transparency
-glEnable(GL_BLEND)
-glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+# Helper functions
+def center_anchor(img):
+    img.anchor_x = img.width // 2
+    img.anchor_y = img.height // 2
 
-image = pyglet.resource.image( 'assets/task.png' )
+# Task Class
+class Task():
+  def __init__(self, image_name, title, context, folder, priority):
+    self.title = image_name
+    self.title = title
+    self.context = context
+    self.folder = folder
+    self.priority = priority
+    self.position = [50,50]
+
+    self.image = pyglet.resource.image( image_name )
+    center_anchor(self.image)
+  
+    label_text = self.title + ', ' + self.context + ', ' + self.folder + ', ' + self.priority 
+  
+    self.label = pyglet.text.Label( label_text,
+				   font_name= 'Sans Serif',
+				   font_size= 12,
+				   x = self.position[0] + self.image.width // 2, 
+				   y = self.position[1],
+				   anchor_x = 'left', anchor_y = 'center' )
+  
+
+  def draw(self):
+    self.position[0] += 1
+    self.position[1] += 1
+    self.image.blit( self.position[0], self.position[1] )
+    self.label.x = self.position[0] + self.image.width // 2
+    self.label.y = self.position[1]
+    self.label.draw()
+				  
 
 @window.event
 def on_draw():
     window.clear()
-    image.blit( 50, 50 )
+    task.draw()
+
+# Setup transparency
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+# Create tasks
+task = Task('assets/task.png',
+	    'Do something',
+	    'Work',
+	    'Project',
+	    '1')
 
 pyglet.app.run()
---
+++ Tests.py
@@ -0,0 +1,11 @@
+names = ['David', 'Chris']
+for name in names:
+    print name
+
+label = ''
+for name in names:
+    label += name + ','
+label = label[0:len(label)-1]
+print label
+    
+
