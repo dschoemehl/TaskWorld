b571c5ca0955d34489a4fe127f4f2cde5f46161a
David Schoemehl <david.schoemehl@asynchrony.com>
2014-07-16 17:43:06 -0500

    Updated task icon, randomly generate several tasks

--- TaskWorld.py
+++ TaskWorld.py
@@ -1,6 +1,16 @@
 import pyglet
 from pyglet.gl import *
 
+import random
+
+CONTEXTS = ['Work', 'Home', 'Phone']
+FOLDERS = ['Project A', 'Project B', 'Project C']
+PRIORITIES = [ 1, 2, 3 ]
+
+LABEL_OFFSET = 0
+TASK_SCALE = 2
+TASK_IMAGE = 'assets/task.png'
+
 window = pyglet.window.Window()
 
 # Helper functions
@@ -16,26 +26,29 @@ class Task():
     self.context = context
     self.folder = folder
     self.priority = priority
-    self.position = [50,50]
+    self.position = [random.randrange(0,window.width,1), random.randrange(0,window.height,1)]
 
     self.image = pyglet.resource.image( image_name )
+    self.image.scale = TASK_SCALE
     center_anchor(self.image)
   
-    label_text = self.title + ', ' + self.context + ', ' + self.folder + ', ' + self.priority 
+    label_text = self.title + '\n' + self.context + '\n' + self.folder + '\n' + str(self.priority) 
   
     self.label = pyglet.text.Label( label_text,
 				   font_name= 'Sans Serif',
-				   font_size= 12,
-				   x = self.position[0] + self.image.width // 2, 
+				   font_size= 8,
+				   x = self.position[0] + self.image.width // 2 + LABEL_OFFSET, 
 				   y = self.position[1],
-				   anchor_x = 'left', anchor_y = 'center' )
+				   anchor_x = 'left', anchor_y = 'center',
+				   width = 100,
+				   multiline = True)
   
 
   def draw(self):
-    self.position[0] += 1
-    self.position[1] += 1
+    #self.position[0] += 1
+    #self.position[1] += 1
     self.image.blit( self.position[0], self.position[1] )
-    self.label.x = self.position[0] + self.image.width // 2
+    self.label.x = self.position[0] + self.image.width // 2 + LABEL_OFFSET
     self.label.y = self.position[1]
     self.label.draw()
 				  
@@ -43,17 +56,33 @@ class Task():
 @window.event
 def on_draw():
     window.clear()
-    task.draw()
+    #task.draw()
+    for theTask in tasks:
+      theTask.draw()
 
 # Setup transparency
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
+CONTEXTS = ['Work', 'Home', 'Phone']
+FOLDERS = ['Project A', 'Project B', 'Project C']
+PRIORITIES = [ 1, 2, 3 ]
+
 # Create tasks
 task = Task('assets/task.png',
 	    'Do something',
-	    'Work',
-	    'Project',
-	    '1')
+	    CONTEXTS[ random.randrange(len(CONTEXTS) - 1) ],
+	    FOLDERS[ random.randrange(len(FOLDERS) - 1) ],
+	    PRIORITIES[ random.randrange(len(PRIORITIES) - 1)])
+
+tasks = []
+count = 0
+while ( count < 30 ):
+  tasks.append( Task( TASK_IMAGE,
+		      'Do something',
+		      CONTEXTS[ random.randrange(len(CONTEXTS) - 1) ],
+		      FOLDERS[ random.randrange(len(FOLDERS) - 1) ],
+		      PRIORITIES[ random.randrange(len(PRIORITIES))] ))
+  count += 1
 
 pyglet.app.run()
